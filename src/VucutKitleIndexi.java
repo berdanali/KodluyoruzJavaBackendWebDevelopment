410bb7e47982b6ad18a34e285b93eb8172a1d89e
lerraytss <berdanali002@gmail.com>
2023-09-18 19:15:34 +0300

    Vucut kitle indexi hesaplama

---
+++ src/VucutKitleIndexi.java
@@ -0,0 +1,16 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class VucutKitleIndexi {
+    public static void main(String[] args) {
+        int kilo;
+        double boy,index;
+        Scanner k = new Scanner(System.in);
+        System.out.println("Lüfen boyunuzu metre cinsinden giriniz ... ");
+        boy = k.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz ... ");
+        kilo = k.nextInt();
+        index = kilo / (boy*boy);
+        System.out.println("Vucut kitle indexsiniz : " + index);
+    }
+}
