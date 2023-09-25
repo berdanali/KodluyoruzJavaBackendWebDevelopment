5fe836c5abada53e5f3067fdd806a1dd6aebb561
lerraytss <berdanali002@gmail.com>
2023-09-25 11:44:38 +0300

    Kucukten buyuge sıralama

---
+++ src/Sıralama.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Sıralama {
+    public static void main(String[] args) {
+        Scanner k= new Scanner(System.in);
+        int num1,num2,num3,max;
+        System.out.println("Lütfen 3 tane sayı giriniz .... ");
+        num1 = k.nextInt();
+        num2 = k.nextInt();
+        num3 = k.nextInt();
+        if(num1>num2 && num1>num3 && num2>num3){
+            System.out.println("Büyükten küçüğe göre sıralama : " + num1 + "-" + num2 + "-" + num3 );
+        }
+        else if(num2>num1 && num2>num3 && num1>num3){
+            System.out.println("Büyükten küçüğe göre sıralama : " + num2 + "-" + num1 + "-" + num3 );
+        }
+        else if(num3>num2 && num3>num1 && num2>num1){
+            System.out.println("Büyükten küçüğe göre sıralama : " + num3 + "-" + num2 + "-" + num1 );
+        }
+        else if(num3>num2 && num3>num1 && num1>num2){
+            System.out.println("Büyükten küçüğe göre sıralama : " + num3 + "-" + num1 + "-" + num2 );
+        }
+        else if(num1>num2 && num1>num3 && num3>num2){
+            System.out.println("Büyükten küçüğe göre sıralama : " + num1 + "-" + num3 + "-" + num2 );
+        }
+        else if(num2>num1 && num2>num3 && num3>num1){
+            System.out.println("Büyükten küçüğe göre sıralama : " + num2 + "-" + num3 + "-" + num1 );
+        }
+    }
+}
