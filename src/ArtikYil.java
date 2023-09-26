05d3bbe74f452772fe88cc4b1df336de9e8fcaf8
lerraytss <berdanali002@gmail.com>
2023-09-26 13:00:09 +0300

    artık yıl hesaplama

---
+++ src/ArtikYil.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        int yil;
+        Scanner k = new Scanner(System.in);
+        System.out.println("Lüfen bir yıl giriniz ... ");
+        yil=k.nextInt();
+        if (yil % 400 == 0 || yil % 4 == 0 && yil % 100 != 0){
+            System.out.println("Bu bir artık yıldır ");
+        }
+        else{
+            System.out.println("Bu bir artık yıl değildir ");
+        }
+
+
+    }
+}
