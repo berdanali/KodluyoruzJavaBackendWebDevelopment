8077b768cacc439c3e1a00cdcce03e78fe8219b4
lerraytss <berdanali002@gmail.com>
2023-10-01 19:04:38 +0300

    Sayıların harmonik serisini bulan program

---
+++ src/Harmonik.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Harmonik {
+    public static void main(String[] args) {
+        int sayi;
+        double total = 0.0;
+        Scanner k = new Scanner(System.in);
+        System.out.println("Lütfen bir sayı giriniz ");
+        sayi = k.nextInt();
+        for(int i = 1 ; i <= sayi ; i++){
+            total+=(1.0/i);
+        }
+        System.out.println(total);
+    }
+}
