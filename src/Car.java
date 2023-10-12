78c69a040ec1cb74a61b745e967e99648e5307c6
lerraytss <berdanali002@gmail.com>
2023-10-12 23:25:17 +0300

    Java Nesneler ve Sınıflar

---
+++ src/Car.java
@@ -0,0 +1,25 @@
+class Car {
+    // nitelikler
+    String type;
+    String model;
+    String color;
+    int speed;
+
+    // davranışlar
+    int increaseSpeed(int increment) {
+        speed += increment;
+        return speed;
+    }
+
+    int decreaseSpeed(int decrease) {
+        if (speed > 0) {
+            speed -= decrease;
+        }
+        return speed;
+    }
+
+    void printSpeed() {
+        System.out.println("Speed : " + speed);
+    }
+
+}
