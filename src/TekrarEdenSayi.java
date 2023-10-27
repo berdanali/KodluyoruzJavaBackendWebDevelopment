6dc6a8aa9a64f000d372197b6637d662a154cffe
lerraytss <berdanali002@gmail.com>
2023-10-27 16:55:37 +0300

    Jvada dizilerde tekrar eden çift sayıları bulan program

---
+++ src/TekrarEdenSayi.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//Bir dizide tekrar eden çift sayıları bulma
+public class TekrarEdenSayi {
+    public static void main(String[] args) {
+
+        int[] array = {3, 335, 35, 546, 65, 5652, 5165, 5165, 525252, 25, 5, 52, 515, 15, 52959, 8, 8, 88, 8848, 88, 8};
+        Arrays.sort(array);
+        System.out.println("Dizi: " + Arrays.toString(array));
+
+        System.out.print("Tekrar Eden Çift Sayılar: ");
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i] == array[i - 1] && array[i] % 2 == 0 && array[i] != array[i + 1]) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+    }
