5e4e371739b56b0254934d9d858a96026fe8adbd
lerraytss <berdanali002@gmail.com>
2023-12-13 14:24:07 +0300

    Palindrom Kelime bulma

--- src/Java101/Palindrom.java
+++ src/Java101/Palindrom.java
@@ -2,20 +2,21 @@
 import java.util.Scanner;
 public class Palindrom {
 
-    static boolean isPalindrom(int number){
-        int temp = number, reverseNumber=0, lastNumber;
-        while (temp != 0){
-            lastNumber = temp %10;
-            reverseNumber = (reverseNumber * 10) + lastNumber;
-            temp /= 10;
+    static boolean isPalindrom(String str){
+        int i = 0;
+        int j = str.length()-1;
+
+        while (i<j){
+           if(str.charAt(i) != str.charAt(j)){
+               return false;
+           }
+           i++;
+           j--;
         }
-        if (number == reverseNumber)
-                return true;
-        else
-            return false;
+            return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrom(8));
+        System.out.println(isPalindrom("kavaak"));
     }
 }
