cc5ffbcd6b27d8bddd6cab4e7f802bb641e1e8cf
lerraytss <berdanali002@gmail.com>
2023-12-24 17:06:35 +0300

    Encapsulation example

--- src/JavaOOP/Main.java
+++ src/JavaOOP/Main.java
@@ -1,5 +1,23 @@
 package JavaOOP;
 
 public class Main {
-
+    // Encapsulation example
+    public class Kitap {
+        private int sayfaSayisi;
+        private String kitapAdi, yazar;
+        Kitap(String kitapAdi, int sayfaSayisi, String yazar) {
+            this.kitapAdi = kitapAdi;
+            this.yazar = yazar;
+        }
+        public int getSayfaSayisi() {
+            return this.sayfaSayisi;
+        }
+        public void setSayfaSayisi(int sayfaSayisi) {
+            if (sayfaSayisi < 1) {
+                this.sayfaSayisi = 10;
+            } else {
+                this.sayfaSayisi = sayfaSayisi;
+            }
+        }
+    }
 }
