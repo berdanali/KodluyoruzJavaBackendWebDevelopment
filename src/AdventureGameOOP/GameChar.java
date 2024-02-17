d8eb72a775705b3d8491d060ba8493aa1d5ad94d
lerraytss <berdanali002@gmail.com>
2024-02-17 14:02:57 +0300

    Karakterler için constructor oluşturuldu

---
+++ src/AdventureGameOOP/GameChar.java
@@ -0,0 +1,57 @@
+public abstract class GameChar {
+    private int id;
+    private int damage;
+    private int health;
+    private int money;
+    private String name;
+
+
+
+    public GameChar(int id, int damage, int health, int money, String name){
+        this.id=id;
+        this.damage=damage;
+        this.health=health;
+        this.money=money;
+        this.name=name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
