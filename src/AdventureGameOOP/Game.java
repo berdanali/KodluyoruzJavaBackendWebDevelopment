618c1248a9886dced4694ab4673b57022940deb7
lerraytss <berdanali002@gmail.com>
2024-02-18 22:36:10 +0300

    Oyunun girişi oluşturuldu

--- src/AdventureGameOOP/Game.java
+++ src/AdventureGameOOP/Game.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
+
 public class Game {
     private Player player;
     private Location location;
 
     public void start(){
+        Scanner input =new Scanner(System.in);
+        System.out.println("Macera oyununa hoşgeldiniz :)");
+        System.out.println("Lütfen değerli isminizi girin");
+        String playerName = input.nextLine();
+        Player player = new Player(playerName);
+        System.out.println(player.getName() + " Macera oyununa hoşgeldin ... ");
+        player.selectChar();
+        Location location=null;
+        while(true){
+            player.printInfo();
+            System.out.println();
+            System.out.println("-------------- Bölgeler  --------------");
+            System.out.println();
+            System.out.println("1-) Güvenli ev  --> Burada düşman yok güvendesiniz bir süreliğine tabi :)");
+            System.out.println("2-) Mağaza  --> Buradan silah veya zırh alabilirsiniz ");
+            System.out.println("Lütfen girmek istediğiniz bölgeyi seçiniz");
+            int selectLocation = input.nextInt();
+            switch (selectLocation){
+                case 1:
+                    location = new SafeHouse(player);
+                    break;
+                case 2:
+                    location = new ToolStore(player);
+                    break;
+                default:
+                    location = new SafeHouse(player);
+            }
+
+           if(!location.onLocation()){
+               System.out.println(" GAME OVER !!! ");
+               break;
+           }
+        }
+
 
     }
 }
--- src/AdventureGameOOP/Main.java
+++ src/AdventureGameOOP/Main.java
@@ -1,8 +1,4 @@
 public class Main{
-
-
  public static void main(String[] args) {
     Game game = new Game();
-    game.start();
- }
-}
+    game.start();}}
