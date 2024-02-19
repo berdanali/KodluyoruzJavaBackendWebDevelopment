c808b6b27701ee362af2def4c44bae763e4b8eae
lerraytss <berdanali002@gmail.com>
2024-02-19 12:11:35 +0300

    Çıkış eklenildi

--- src/AdventureGameOOP/Game.java
+++ src/AdventureGameOOP/Game.java
@@ -20,9 +20,13 @@ public class Game {
             System.out.println();
             System.out.println("1-) Güvenli ev  --> Burada düşman yok güvendesiniz bir süreliğine tabi :)");
             System.out.println("2-) Mağaza  --> Buradan silah veya zırh alabilirsiniz ");
+            System.out.println("0-) Çıkış yap --> Oyunu sonlandır");
             System.out.println("Lütfen girmek istediğiniz bölgeyi seçiniz");
             int selectLocation = input.nextInt();
             switch (selectLocation){
+                case 0:
+                    location=null;
+                    break;
                 case 1:
                     location = new SafeHouse(player);
                     break;
@@ -32,7 +36,10 @@ public class Game {
                 default:
                     location = new SafeHouse(player);
             }
-
+            if(location==null){
+                System.out.println("Oyun bitti yine bekleriz ");
+                break;
+            }
            if(!location.onLocation()){
                System.out.println(" GAME OVER !!! ");
                break;
