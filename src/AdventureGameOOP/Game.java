3895f999bd38ea01d7d4cb7fca87fecea605863a
lerraytss <berdanali002@gmail.com>
2024-02-20 12:51:18 +0300

    Bölge seçme dinamiği getirildi

--- src/AdventureGameOOP/Game.java
+++ src/AdventureGameOOP/Game.java
@@ -10,6 +10,7 @@ public class Game {
         System.out.println("Lütfen değerli isminizi girin");
         String playerName = input.nextLine();
         Player player = new Player(playerName);
+        System.out.println("------------------------------------------------------------------------------");
         System.out.println(player.getName() + " Macera oyununa hoşgeldin ... ");
         player.selectChar();
         Location location=null;
@@ -19,7 +20,11 @@ public class Game {
             System.out.println("-------------- Bölgeler  --------------");
             System.out.println();
             System.out.println("1-) Güvenli ev  --> Burada düşman yok güvendesiniz bir süreliğine tabi :)");
-            System.out.println("2-) Mağaza  --> Buradan silah veya zırh alabilirsiniz ");
+            System.out.println("2-) Eşya Dükkanı  --> Buradan silah veya zırh alabilirsiniz ");
+            System.out.println("3-) Mağara --> Dikkatli ol zombi çıkabilir :) Ödülün : *YEMEK*");
+            System.out.println("4-) Orman --> Dikkatli ol vampir çıkabilir :) Ödülün : *ODUN*");
+            System.out.println("5-) Nehir --> Dikkatli ol ayı çıkabilir :) Ödülün : *SU*");
+
             System.out.println("0-) Çıkış yap --> Oyunu sonlandır");
             System.out.println("Lütfen girmek istediğiniz bölgeyi seçiniz");
             int selectLocation = input.nextInt();
@@ -33,8 +38,17 @@ public class Game {
                 case 2:
                     location = new ToolStore(player);
                     break;
+                case 3:
+                    location = new Cave(player);
+                    break;
+                case 4:
+                    location = new Forest(player);
+                    break;
+                case 5:
+                    location = new River(player);
+                    break;
                 default:
-                    location = new SafeHouse(player);
+                    System.out.println("Lütfen geçerli bir değer giriniz ");
             }
             if(location==null){
                 System.out.println("Oyun bitti yine bekleriz ");
